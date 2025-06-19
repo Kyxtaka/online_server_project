279154f50651f1930cd944a319df0c1e3f2b1826
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-06-19 20:12:53 +0200

    modified table names and remove some columns

--- database/creation.sql
+++ database/creation.sql
@@ -2,12 +2,12 @@
 -- This script creates the database and the tables for the application
 
 -- Drop the database if it exists
-DROP DATABASE IF EXISTS `ONLINESERVICSEUTILITIESDB`;
+DROP DATABASE IF EXISTS `SERVMANAGEMENT`;
 -- Create the database  
-CREATE DATABASE `ONLINESERVICESUTILITIESDB` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE `SERVMANAGEMENT` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- Use the created database
-USE `ONLINESERVICESUTILITIESDB`;
+USE `SERVMANAGEMENT`;
 
 -- Create the users table
 CREATE TABLE IF NOT EXISTS `USERS` (
@@ -19,20 +19,20 @@ CREATE TABLE IF NOT EXISTS `USERS` (
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `LIVECOMPUTER` (
-    `macaddress` VARCHAR(17) NOT NULL PRIMARY KEY,
-    `ipaddress` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `COMPUTER` (
+    `macAddress` VARCHAR(17) NOT NULL PRIMARY KEY,
+    `localV4IpAddress` VARCHAR(50) NOT NULL,
+    `v6IpAddress` VARCHAR(50) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `hostname` VARCHAR(255) NOT NULL,
     `os` VARCHAR(50) NOT NULL,
     `network` VARCHAR(50) NOT NULL,
-    `status` ENUM('online', 'offline') NOT NULL DEFAULT 'offline',
     `lastseen` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `USERCOMPUTERACCESS`(
     `email` VARCHAR(50) NOT NULL REFERENCES `USERS`(`email`) ON DELETE CASCADE,
-    `macaddress` VARCHAR(17) NOT NULL REFERENCES `LIVECOMPUTER`(`macaddress`) ON DELETE CASCADE,
-    `systemauthoritylevel` ENUM('admin', 'user') NOT NULL DEFAULT 'user',
-    PRIMARY KEY (`email`, `macaddress`)
+    `macAddress` VARCHAR(17) NOT NULL REFERENCES `COMPUTER`(`macAddress`) ON DELETE CASCADE,
+    `systemAuthorityLevel` ENUM('ADMIN', 'FRIENDS', 'GUEST') NOT NULL DEFAULT 'GUEST',
+    PRIMARY KEY (`email`, `macAddress`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;

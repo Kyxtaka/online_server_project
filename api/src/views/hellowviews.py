e374d844a0ad5f1ffbc21d1d56a11b942484b1aa
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-06-20 16:56:35 +0200

    edit files trees

--- api/src/views/hellowviews.py
+++
@@ -1,8 +0,0 @@
-from flask_restx import Resource, Namespace
-
-ns = Namespace("hello", description="Hello World API")
-@ns.route("/hw")
-class HelloWorld(Resource):
-    def get(self):
-        return {"message": "Hello, World!"}
-    

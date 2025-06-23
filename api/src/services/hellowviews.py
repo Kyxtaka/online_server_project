a1e49a66ae90edd1da333f62cb96820623ea54af
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-06-23 11:56:30 +0200

    refactoring role decorators inside model folder

--- api/src/services/hellowviews.py
+++
@@ -1,9 +0,0 @@
-from flask_restx import Resource, Namespace
-
-hello_ns = Namespace("hello", description="Hello World API")
-
-@hello_ns.route("/hw")
-class HelloWorld(Resource):
-    def get(self):
-        return {"message": "Hello, World!"}
-    

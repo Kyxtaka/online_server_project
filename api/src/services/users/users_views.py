c76cfde4fd205e89411d176bf71cdea48d9b57fb
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-09-05 23:52:05 +0200

    implement str upper / lower no diff

--- api/src/services/users/users_views.py
+++ api/src/services/users/users_views.py
@@ -1,10 +1,11 @@
 from flask_restx import Resource, Namespace, fields
 from src.model.datamodel.entityModel import user_model, user_input_model, usercomputer_access_model, usercomputer_access_input_model
-from src.model.datamodel.entityORM import Users, UserComputerRights, Computers, UsersCRUD, AppRoleList
+from src.model.datamodel.entityORM import Users, UsersCRUD, AppRoleList
 from src.extensions import api
 from src.app import api_version_path
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from src.model.auth.auth_decorators import admin_required
+from src.services.users.users_controller import check_role
 
 user_ns:Namespace = Namespace("user", description="User management API", path=api_version_path + "/user", decorators=[api.doc(security='BearerAuth')])
 
@@ -14,13 +15,17 @@ class UserCollection(Resource):
     @user_ns.marshal_with(user_model)
     @admin_required
     def post(self):
+        try: 
+            check_role(user_ns.payload['role'], AppRoleList)
+        except ValueError as e:
+            return {"msg": "Bad request - "+e}, 400
         print("Creating a new user with payload:", user_ns.payload)
         if 'password' not in user_ns.payload:
             return {"msg": "Password is required"}, 400
         user = UsersCRUD.create(
-            username=user_ns.payload['username'],
-            email=user_ns.payload['email'],
-            role=user_ns.payload['role'],
+            username=user_ns.payload['username'].lower(),
+            email=user_ns.payload['email'].lower(),
+            role=user_ns.payload['role'].upper(),
             password=user_ns.payload['password']
         )
         return user, 201
@@ -37,12 +42,16 @@ class UserItem(Resource):
     @user_ns.expect(user_input_model)
     @user_ns.marshal_with(user_model)
     def put(self, id):
+        try: 
+            check_role(user_ns.payload['role'], AppRoleList)
+        except ValueError as e:
+            return {"msg": "Bad request - "+e}, 400
         match UsersCRUD.get_by_email(get_jwt_identity()).role: 
             case AppRoleList.ADMIN:
                 user = UsersCRUD.update(
-                    id, username=user_ns.payload['username'],
-                    email=user_ns.payload['email'],
-                    role=user_ns.payload['role'],
+                    id, username=user_ns.payload['username'].lower(),
+                    email=user_ns.payload['email'].lower(),
+                    role=user_ns.payload['role'].upper(),
                     password=user_ns.payload['password']
                 )
             case AppRoleList.USER:
@@ -50,8 +59,8 @@ class UserItem(Resource):
                 if current_user.id != id:
                     return {"msg": "You can only update your own user information"}, 403
                 user = UsersCRUD.update(
-                    id, username=user_ns.payload['username'],
-                    email=user_ns.payload['email'],
+                    id, username=user_ns.payload['username'].lower(),
+                    email=user_ns.payload['email'].lower(),
                     role=current_user.role,  # User cannot change their own role
                     password=user_ns.payload['password']
                 )

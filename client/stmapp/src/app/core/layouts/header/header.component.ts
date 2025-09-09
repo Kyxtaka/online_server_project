6902b7a3628e5a22d902aea67f137fc14fa44c86
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-09-09 15:26:10 +0200

    nav bar header - global layout

--- client/stmapp/src/app/core/layouts/header/header.component.ts
+++ client/stmapp/src/app/core/layouts/header/header.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user/user.service';
+import { AuthService } from '../../services/auth/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  public isLogged: boolean = false;
+  public username: string = "None";
+  
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+      this.isLogged = this.authService.isLoggedIn();
+  }
 
 }

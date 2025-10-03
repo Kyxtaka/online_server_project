44a9c6f8af40d638723f262a504a1e0e07e31bba
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-10-03 08:05:30 +0200

    edit config docker compose, test ci

--- api/src/app.py
+++ api/src/app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 app = Flask(__name__)
 
-dotentv_path = join(dirname(__file__), '..','.flaskenv')
+dotentv_path = join(dirname(__file__), '..','.env')
 load_dotenv(dotenv_path=dotentv_path)
 prod = False
 if os.getenv("ENVIRONMENT") != "dev": prod = True

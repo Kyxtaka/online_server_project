63e3308e89ab24e5809adab4999240e36dbbdcba
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-11-02 22:11:10 +0100

    Use CookieService to retrieve JWT token
    Replaces the tokenGetter in JwtModule configuration to use ngx-cookie-service for retrieving the token from cookies instead of AuthService. This change centralizes token management and may improve compatibility with cookie-based authentication flows.

--- client/stmapp/src/app/app.config.ts
+++ client/stmapp/src/app/app.config.ts
@@ -9,6 +9,7 @@ import { routes } from './app.routes';
 import { AuthService } from './core/services/auth/auth.service';
 
 import { environment } from '../environments/environment';
+import { CookieService } from 'ngx-cookie-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,7 +20,12 @@ export const appConfig: ApplicationConfig = {
       JwtModule.forRoot(
         {
           config: {
-            tokenGetter: () => inject(AuthService).getToken(),
+            // tokenGetter: () => inject(AuthService).getToken(),
+            tokenGetter: () => {
+              const cookieService = inject(CookieService);
+              const token: string | null = cookieService.get('token_name');
+              return token;
+            },
             // allowedDomains: [environment.API_DOMAIN],
             allowedDomains: ["stm-api.hikarizsu.fr", "stm-api-test.hikarizsu.fr"],
             disallowedRoutes: [
